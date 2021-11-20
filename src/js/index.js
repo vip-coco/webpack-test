b4fa1e7fa99081629d7081c8e25008ef17f17256
liuchaofu <liuchaofu@uniondrug.cn>
2021-11-21 02:13:25 +0800

    workbox-webpack-plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
-import $ from 'jquery';
+/* 
+    sw代码必须运行在服务器上
+      --> nodejs 
+        npm i serve -g
+        serve -s build 启动服务器，将build目录下所有资源作为静态资源暴露出去
+*/
+// 注册serviceWorker
+// 处理兼容性问题
 
-$('#title').html("Hello")
-//console.log($);
- 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("sw注册成功了 ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW注册失败 ", registrationError);
+      });
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
-const { resolve } = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'js/built.js',
-    path: resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build')
   },
   plugins: [
+    new WorkboxWebpackPlugin.GenerateSW({
+      /*
+        1. 帮助serviceworker快速启动
+        2. 删除旧的 serviceworker
+        生成一个 serviceworker 配置文件~
+        https://webpack.docschina.org/guides/progressive-web-application/#adding-workbox
+      */
+      clientsClaim: true,
+      skipWaiting: true
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
   ],
-  mode: 'production',
-  externals: {
-    // 拒绝jQuery被打包进来
-    jquery: 'jQuery'
-  }
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 1000,
+    open: true, 
+  }, 
+  mode: 'development',
 };
